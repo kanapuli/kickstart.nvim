23fedbccb5715dc0d92bb0f3543eb7cfaa294481
Athavan Kanapuli <athavankanapuli@gmail.com>
2025-04-14 08:24:22 +0200

    Add persistence keymap

--- lua/athavan/keymaps.lua
+++ lua/athavan/keymaps.lua
@@ -45,3 +45,24 @@ end, { noremap = true, silent = true })
 vim.keymap.set('n', '<localleader>cc', function()
   require('codecompanion').chat {}
 end, { noremap = true, silent = true })
+
+-- Persistence keymaps
+-- load the session for the current directory
+vim.keymap.set('n', '<leader>qs', function()
+  require('persistence').load()
+end, { desc = 'Load the session from the current directory' })
+
+-- select a session to load
+vim.keymap.set('n', '<leader>qS', function()
+  require('persistence').select()
+end, { desc = 'Select a persistence session ' })
+
+-- load the last session
+vim.keymap.set('n', '<leader>ql', function()
+  require('persistence').load { last = true }
+end, { desc = 'Load the last session' })
+
+-- stop Persistence => session won't be saved on exit
+vim.keymap.set('n', '<leader>qd', function()
+  require('persistence').stop()
+end, { desc = "Don't save the session" })
